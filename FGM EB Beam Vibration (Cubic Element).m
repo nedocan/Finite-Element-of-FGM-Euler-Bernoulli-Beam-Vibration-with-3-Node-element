ccfcc6bd7f56ff3e2e7ec0bc9742fccffa7aef43
Nedimcan Aytemür <95765213+nedocan@users.noreply.github.com>
2021-12-08 19:18:14 +0300

    Update FGM EB Beam Vibration (Cubic Element).m

--- FGM EB Beam Vibration (Cubic Element).m
+++ FGM EB Beam Vibration (Cubic Element).m
@@ -5,8 +5,8 @@ syms x L c1 c2 c3 c4 c5 c6 c7 c8 c9...
     W1 W2 W3 Teta1 Teta2 Teta3 U1 U2 U3...
     A11 B11 D11 I0 I1 I2 z real
 
-w=c1+c2*x+c3*x^2+c4*x^3+c5*x^4+c6*x^5;
-u=c7+c8*x+c9*x^2;
+w=c1+c2*x+c3*x^2+c4*x^3+c5*x^4+c6*x^5; %% Interpolation function of w
+u=c7+c8*x+c9*x^2; %% Interpolation function of u
 
 u1=subs(u,0);
 w1=subs(w,0);
@@ -38,18 +38,18 @@ u=collect(c(7)+c(8)*x+c(9)*x^2,q);
 
 [N_u_local,terms]=coeffs(u,q);
 [N_w_local,terms]=coeffs(w,q);
-N_w=[0 N_w_local(1) N_w_local(2) 0 N_w_local(3) N_w_local(4) 0 N_w_local(5) N_w_local(6)];
-N_u=[N_u_local(1) 0 0 N_u_local(2) 0 0 N_u_local(3) 0 0];
+N_w=[0 N_w_local(1) N_w_local(2) 0 N_w_local(3) N_w_local(4) 0 N_w_local(5) N_w_local(6)]; %% Shape function of w
+N_u=[N_u_local(1) 0 0 N_u_local(2) 0 0 N_u_local(3) 0 0]; %% Shape function of u
 
 K_u=A11*int(diff(N_u,x)'*diff(N_u,x),x,[0,L]);
 K_w=D11*int(diff(N_w,x,x)'*diff(N_w,x,x),x,[0,L]);
 K_u_w=-B11*int(diff(N_u,x)'*diff(N_w,x,x)+diff(N_w,x,x)'*diff(N_u,x),x,[0,L]);
-K=K_u+K_w+K_u_w;
+K=K_u+K_w+K_u_w; %% Local stiffness matrix (symbolic)
 
 M_u=I0*int(N_u'*N_u,x,[0,L]);
 M_w=I0*int(N_w'*N_w,x,[0,L])+I2*int(diff(N_w,x)'*diff(N_w,x),x,[0,L]);
 M_u_w=-I1*int(N_u'*diff(N_w,x)+diff(N_w,x)'*N_u,x,[0,L]);
-M=M_u+M_w+M_u_w;
+M=M_u+M_w+M_u_w; %% Local mass matrix (symbolic)
 
 %%%% Geometry %%%%%
 h=  0.1; %height of beam
@@ -70,7 +70,7 @@ E_ceramic=380*10^9;
 rho_ceramic=3800;
 nu_ceramic=0.14;
 
-%%%%%  Case; upper face is ceramic; lower face is metallic %%%%%
+%%%%%  Case; upper face is ceramic; lower face is metallic (Effective material Properties) %%%%%
 E_effective=(E_ceramic-E_metallic)*(z/h+0.5)^k+E_metallic;
 rho_effective=(rho_ceramic-rho_metallic)*(z/h+0.5)^k+rho_metallic;
 nu_effective=(nu_ceramic-nu_metallic)*(z/h+0.5)^k+nu_metallic;
@@ -89,17 +89,17 @@ R=[A11_numeric,B11_numeric,D11_numeric]; %% Coefficinet of Stress Resultants
 
 nel=5;  %%Number of Divided Elements
 
-mm=double(subs(M,{A11,B11,D11,I0,I1,I2,L},{R(1),R(2),R(3),I(1),I(2),I(3),l/nel}));
-kk=double(subs(K,{A11,B11,D11,I0,I1,I2,L},{R(1),R(2),R(3),I(1),I(2),I(3),l/nel}));
+mm=double(subs(M,{A11,B11,D11,I0,I1,I2,L},{R(1),R(2),R(3),I(1),I(2),I(3),l/nel})); %% Local mass matrix 
+kk=double(subs(K,{A11,B11,D11,I0,I1,I2,L},{R(1),R(2),R(3),I(1),I(2),I(3),l/nel})); %% Local stiffness matrix 
 
 n_nodes=2*nel+1;  %%Node Number
 Dof=3;  %%Degree of Freedom of a Node
 
 %%% Boundary Conditions %%%
-BCs=[1,2,3,n_nodes*Dof-2,n_nodes*Dof-1,n_nodes*Dof]; % C-C
-%BCs=[1,2,n_nodes*Dof-2,n_nodes*Dof-1]; % S-S
-%BCs=[1,2,3,n_nodes*Dof-2,n_nodes*Dof-1]; %C-S
-%BCs=[1,2,3]; % C-F
+BCs=[1,2,3,n_nodes*Dof-2,n_nodes*Dof-1,n_nodes*Dof]; % Clamped-Clamped
+%BCs=[1,2,n_nodes*Dof-2,n_nodes*Dof-1]; % Simply Supported-Simply Supported
+%BCs=[1,2,3,n_nodes*Dof-2,n_nodes*Dof-1]; %Clamped-Simply Supported
+%BCs=[1,2,3]; % Clamped-Free
 
 
 
@@ -110,7 +110,7 @@ Mglob=zeros(Dof*n_nodes,Dof*n_nodes)  ;
 for i=1:nel
     top(i,:)=[6*i-5 6*i-4 6*i-3 6*i-2 6*i-1 6*i 6*i+1 6*i+2 6*i+3]; %% Topology Matrix
 end
-
+%%% Assembly Process  %%%
 for n=1:nel
     for i=1:9
         for j=1:9
@@ -129,14 +129,13 @@ for n=1:nel
     end
 end
 
-activeDof=setdiff(1:Dof*n_nodes',BCs);
+activeDof=setdiff(1:Dof*n_nodes',BCs); %% Rows and columns are omittid which are boundary conditions
 
 Kglobactive=Kglob(activeDof,activeDof);
 Mglobactive=Mglob(activeDof,activeDof);
 
-%System_Matrix=(inv(Mglobactive)*Kglobactive);
-[vectorc,freqc]=eig(Kglobactive,Mglobactive);
+[vectorc,freqc]=eig(Kglobactive,Mglobactive); %% Eigenvalue problem
 nat_freq=freqc;
 freqc=diag(freqc);
-freqc=sort(sqrt(freqc))
+freqc=sort(sqrt(freqc)) %% natural frequencies
 
